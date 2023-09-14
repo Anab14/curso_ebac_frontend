61738cd8b59504c7bc1efe20d979e5030f538a74
Anab14 <131063828+Anab14@users.noreply.github.com>
2023-09-14 20:25:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
     $('form').on('submit', function(e) {
         e.preventDefault();
         const nometarefa = $('#nome-tarefa').val();
-        const novoitem = $('<li></li>');
+        const novoitem = $("<li>"+nometarefa+"</li>");
         $(novoitem).appendTo('ul');
         $(nometarefa).appendTo(novoitem);
+        $(novoitem).click(function() {
+            $(novoitem).addClass("traco");
+        });
     })
 })
